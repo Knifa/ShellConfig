38d19ab1d1221051862579b330d256579df32a5d
Daniel Callander <daniel.callander@skyscanner.net>
2015-12-04 17:53:24 +0000

    add tmux config

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ cwd=$(readlink -f $(dirname "$0"))
 ln -sf "$cwd/zshrc" "$HOME/.zshrc"
 ln -sf "$cwd/dircolors" "$HOME/.dircolors"
 ln -sf "$cwd/oh-my-zsh-custom" "$HOME/.oh-my-zsh-custom"
+ln -sf "$cwd/tmux.conf" "$HOME/.tmux.conf"
